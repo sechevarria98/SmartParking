efc50ee0480f331726000569535f267236a3c1ac
sechevarria98 <samechevarria2@gmail.com>
2020-12-03 22:54:21 -0600

    overhaul of the login and dblogin files

--- src/DBLogin.java
+++ src/DBLogin.java
@@ -1,124 +1,12 @@
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.*;
-
-public class DBLogin extends JFrame {
-
-	private JPanel contentPane;	
-	private JTextField url1;
-	private JTextField usr;
-	private JPasswordField pass;
+public class DBLogin {
 	
 	static String url;
 	static String user;
 	static String password;
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					DBLogin frame = new DBLogin();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	public DBLogin() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 348, 481);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(0, 0, 332, 442);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		usr = new JTextField();
-		usr.setBackground(Color.WHITE);
-		usr.setFont(new Font("Trebuchet MS", Font.PLAIN, 11));
-		usr.setBounds(43, 100, 202, 20);
-		panel_1.add(usr);
-		usr.setColumns(10);
-		
-		pass = new JPasswordField();
-		pass.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 11));
-		pass.setBackground(Color.WHITE);
-		pass.setBounds(43, 181, 202, 20);
-		panel_1.add(pass);
-		
-		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setFont(new Font("Segoe UI Black", Font.BOLD, 13));
-		lblUsername.setBounds(43, 75, 86, 14);
-		panel_1.add(lblUsername);
-		
-		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setFont(new Font("Segoe UI Black", Font.BOLD, 13));
-		lblPassword.setBounds(43, 156, 86, 14);
-		panel_1.add(lblPassword);
-		
-		JLabel lblUrl = new JLabel("Url:");
-		lblUrl.setFont(new Font("Segoe UI Black", Font.BOLD, 13));
-		lblUrl.setBounds(43, 237, 86, 14);
-		panel_1.add(lblUrl);
-		
-		url1 = new JTextField();
-		url1.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 11));
-		url1.setBounds(43, 262, 202, 20);
-		panel_1.add(url1);
-		url1.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Ex. postgres");
-		lblNewLabel.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 10));
-		lblNewLabel.setBounds(43, 131, 76, 14);
-		panel_1.add(lblNewLabel);
-		
-		JLabel lblExPassword = new JLabel("Ex. password");
-		lblExPassword.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 10));
-		lblExPassword.setBounds(43, 212, 76, 14);
-		panel_1.add(lblExPassword);
-		
-		JLabel lblEx = new JLabel("Ex. jdbc:postgresql://localhost/OnlineGroceryStore");
-		lblEx.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 10));
-		lblEx.setBounds(43, 293, 255, 14);
-		panel_1.add(lblEx);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setBackground(Color.WHITE);
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				
-				url = url1.getText();
-				user = usr.getText();
-				password = pass.getText();
-				
-				try (Connection conn = DriverManager.getConnection(url, user, password)){
-					System.out.println("Connected to the PostgreSQL server successfully.");
-					
-					Login log = new Login();
-					log.setVisible(true);
-					dispose();
-					
-				}catch (SQLException ev) {
-					System.out.println(ev.getMessage());
-			   	}
-			}
-		});
-		btnLogin.setFont(new Font("Trebuchet MS", Font.PLAIN, 11));
-		btnLogin.setBounds(110, 343, 89, 37);
-		panel_1.add(btnLogin);
-		
+		url = "jdbc:postgresql://localhost/<DATABASE_NAME>";
+		user = "postgres"; 
+		password = "*******************";
 	}	
-	
 }
--- src/Login.java
+++ src/Login.java
@@ -23,26 +23,36 @@ import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
 
 public class Login extends JFrame {
-
+	//Panel
 	private JPanel contentPane;
-	private JTextField name;
-	private JTextField createUserN;
-	private JTextField usr;
-	private JLabel createNamelbl;
-	private JLabel createUsrNamelbl;
-	private JLabel createPasslbl;
-	private JLabel lblName;
-	private JLabel lblPass;
+
+	//Text Fields
+	private JTextField nameTextField;
+	private JTextField createuserTextField;
+	private JTextField userTextField;
+
+	//Labels
+	private JLabel nameLabel;
+	private JLabel createusernameLabel;
+	private JLabel createpasswordLabel;
+	private JLabel usernameLabel;
+	private JLabel passwordLabel;
+	private JLabel createLabel
+	private JLabel welcomeLabel
+
+	//Password Fields
 	private JPasswordField pass;
-	private JPasswordField createPass;
+	private JPasswordField newPassword;
 
 	
 	DBLogin lg = new DBLogin();
 	String url = lg.url;
 	String user = lg.user;
 	String password = lg.password;
-	static String userName = "";
-	static String cUserName = "";
+
+	static String currentUser = null;
+	private String currentName = null;
+	private String currentPassword;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -74,126 +84,92 @@ public class Login extends JFrame {
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		/*
-		 * Labels
-		 */
+		//======================== LABELS ========================//
 		
-		JLabel lblNewLabel = new JLabel("Welcome Back!");
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
-		lblNewLabel.setBounds(86, 103, 169, 27);
-		panel.add(lblNewLabel);
+		welcomeLabel = new JLabel("Welcome Back!");
+		welcomeLabel.setForeground(Color.WHITE);
+		welcomeLabel.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
+		welcomeLabel.setBounds(86, 103, 169, 27);
+		panel.add(welcomeLabel);
 		
-		JLabel lblCreateAccount = new JLabel("Create Account");
-		lblCreateAccount.setForeground(Color.BLACK);
-		lblCreateAccount.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
-		lblCreateAccount.setBounds(432, 98, 175, 27);
-		contentPane.add(lblCreateAccount);
+		createLabel = new JLabel("Create Account");
+		createLabel.setForeground(Color.BLACK);
+		createLabel.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
+		createLabel.setBounds(432, 98, 175, 27);
+		contentPane.add(createLabel);
 		
-		lblName = new JLabel("User Name:");
-		lblName.setForeground(Color.WHITE);
-		lblName.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
-		lblName.setBounds(86, 168, 82, 20);
-		panel.add(lblName);
+		usernameLabel = new JLabel("User Name:");
+		usernameLabel.setForeground(Color.WHITE);
+		usernameLabel.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
+		usernameLabel.setBounds(86, 168, 82, 20);
+		panel.add(usernameLabel);
 		
-		lblPass = new JLabel("Password:");
-		lblPass.setForeground(Color.WHITE);
-		lblPass.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
-		lblPass.setBounds(86, 219, 82, 20);
-		panel.add(lblPass);
+		passwordLabel = new JLabel("Password:");
+		passwordLabel.setForeground(Color.WHITE);
+		passwordLabel.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
+		passwordLabel.setBounds(86, 219, 82, 20);
+		panel.add(passwordLabel);
 		
-		createNamelbl = new JLabel("Name:");
-		createNamelbl.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
-		createNamelbl.setBounds(432, 154, 82, 20);
-		contentPane.add(createNamelbl);
+		nameLabel = new JLabel("Name:");
+		nameLabel.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
+		nameLabel.setBounds(432, 154, 82, 20);
+		contentPane.add(nameLabel);
 		
-		createUsrNamelbl = new JLabel("User Name:");
-		createUsrNamelbl.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
-		createUsrNamelbl.setBounds(432, 193, 82, 20);
-		contentPane.add(createUsrNamelbl);
+		createusernameLabel = new JLabel("User Name:");
+		createusernameLabel.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
+		createusernameLabel.setBounds(432, 193, 82, 20);
+		contentPane.add(createusernameLabel);
 		
-		createPasslbl = new JLabel("Password:");
-		createPasslbl.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
-		createPasslbl.setBounds(432, 235, 82, 20);
-		contentPane.add(createPasslbl);
+		createpasswordLabel = new JLabel("Password:");
+		createpasswordLabel.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
+		createpasswordLabel.setBounds(432, 235, 82, 20);
+		contentPane.add(createpasswordLabel);
 		
-		/*
-		 * End
-		 */
+		//======================== END ========================//
 		
-		/*
-		 * Text field
-		 */
+		//======================== TEXTFIELDS ========================//
 		
-		usr = new JTextField();
-		usr.setFont(new Font("Trebuchet MS", Font.PLAIN, 11));
-		usr.setColumns(10);
-		usr.setBackground(Color.WHITE);
-		usr.setBounds(86, 188, 146, 20);
-		panel.add(usr);
+		userTextField = new JTextField();
+		userTextField.setFont(new Font("Trebuchet MS", Font.PLAIN, 11));
+		userTextField.setColumns(10);
+		userTextField.setBackground(Color.WHITE);
+		userTextField.setBounds(86, 188, 146, 20);
+		panel.add(userTextField);
 		
-		name = new JTextField();
-		name.setBackground(Color.WHITE);
-		name.setBounds(432, 173, 146, 20);
-		contentPane.add(name);
-		name.setColumns(10);
+		nameTextField = new JTextField();
+		nameTextField.setBackground(Color.WHITE);
+		nameTextField.setBounds(432, 173, 146, 20);
+		contentPane.add(nameTextField);
+		nameTextField.setColumns(10);
 		
-		createUserN = new JTextField();
-		createUserN.setBackground(Color.WHITE);
-		createUserN.setColumns(10);
-		createUserN.setBounds(432, 213, 146, 20);
-		contentPane.add(createUserN);
+		createuserTextField = new JTextField();
+		createuserTextField.setBackground(Color.WHITE);
+		createuserTextField.setColumns(10);
+		createuserTextField.setBounds(432, 213, 146, 20);
+		contentPane.add(createuserTextField);
 		
 		pass = new JPasswordField();
 		pass.setBackground(Color.WHITE);
 		pass.setBounds(86, 240, 146, 20);
 		panel.add(pass);
 		
-		createPass = new JPasswordField();
-		createPass.setBackground(Color.WHITE);
-		createPass.setBounds(432, 252, 146, 20);
-		contentPane.add(createPass);
+		newPassword = new JPasswordField();
+		newPassword.setBackground(Color.WHITE);
+		newPassword.setBounds(432, 252, 146, 20);
+		contentPane.add(newPassword);
 		
-		/*
-		 * End
-		 */
+		//======================== END ========================//
 		
 		
-		/*
-		 * Buttons
-		 */
+		//======================== BUTTONS ========================//
 		
 		JButton SignUpBtn = new JButton("Sign Up");
 		SignUpBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try (Connection conn = DriverManager.getConnection(url, user, password)) {
-					
-					cUserName = createUserN.getText();
-					String query = "SELECT * FROM userLogin WHERE username = '" + createUserN.getText() + "'";
-					
-					Statement s = conn.createStatement();
-					ResultSet r = s.executeQuery(query);
-					
-					if(r.next()) {
-						JOptionPane.showMessageDialog(null, "User name already taken");
-					}
-					else {
-						String query_1 = "INSERT INTO userLogin(name, username, password) VALUES( '" + name.getText() + "','" + createUserN.getText() + "', '" + createPass.getText() + "'" + ");";
-						Statement st = conn.createStatement();
-						st.execute(query_1);
-						
-						name.setText("");
-						createUserN.setText("");
-						createPass.setText("");
-						
-						PostLogin lg = new PostLogin();
-						lg.setVisible(true);
-						dispose();
-					}
-					
-				}catch (SQLException ev) {
-					System.out.println(ev.getMessage());
-				}
+				currentUser = createuserTextField.getText();
+				currentPassword = newPassword.getText();
+				currentName = nameTextField.getText();
+				onSignUp(currentUser, currentPassword, currentName);
 			}
 		});
 		SignUpBtn.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
@@ -204,30 +180,9 @@ public class Login extends JFrame {
 		JButton SignInBtn = new JButton("Sign In");
 		SignInBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try (Connection conn = DriverManager.getConnection(url, user, password)) {
-					
-					userName = usr.getText();
-					String query = "SELECT * FROM userLogin WHERE username = '" + userName + "'";
-					String uPass = pass.getText();
-					Statement s = conn.createStatement();
-					ResultSet r = s.executeQuery(query);
-					
-					if(r.next()) {
-						if (uPass.equals(r.getString("password"))) {
-							Member mem = new Member();
-							mem.setVisible(true);
-							dispose();
-						}
-						else {
-							JOptionPane.showMessageDialog(null, "Password is incorrect");
-						}
-					}
-					else {
-						JOptionPane.showMessageDialog(null, "Username is incorrect");
-					}
-				}catch (SQLException ev) {
-					System.out.println(ev.getMessage());
-				}
+				currentUser = userTextField.getText();
+				currentPassword = pass.getText();
+				onLogin(currentUser, currentPassword);
 			}
 		});
 		SignInBtn.setBorder(new EmptyBorder(0, 0, 0, 0));
@@ -237,15 +192,65 @@ public class Login extends JFrame {
 		panel.add(SignInBtn);
 		
 		
-		/*
-		 * End
-		 */
+		//======================== END ========================//
+	}
+
+	private void onLogin(String tmp_user, String tmp_pass) {
+		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+					
+			// query string for DB
+			String query = "SELECT * FROM userLogin WHERE username = '" + tmp_user + "'";
+			Statement s = conn.createStatement();
+			ResultSet r = s.executeQuery(query);
+			
+			if(r.next()) {
+				// checking if current row column matches the password
+				if (tmp_pass.equals(r.getString("password"))) {
+					Member mem = new Member();
+					mem.setVisible(true);
+					dispose();
+				}
+				else {
+					// matched user name in the DB but not the password
+					JOptionPane.showMessageDialog(null, "Password is incorrect");
+				}
+			}
+			else {
+				// user name wasn't in the database
+				JOptionPane.showMessageDialog(null, "Username is incorrect");
+			}
+		}catch (SQLException err) {
+			System.out.println(err.getMessage());
+		}
+	}
+
+	private void onSignUp(String tmp_user, String tmp_password, , String tmp_name) {
+		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+			// verify that this username doesen't already exist in the database
+			String verify_username_query = "SELECT * FROM userLogin WHERE username = '" + tmp_user + "'";
+			Statement s = conn.createStatement();
+			ResultSet r = s.executeQuery(verify_username_query);
+			
+			if(r.next()) {
+				JOptionPane.showMessageDialog(null, "This username has already been taken.");
+				return;
+			}
+
+			// reaching this point insert into the database
+			String newuser_query = "INSERT INTO userLogin(name, username, password) VALUES( '" + tmp_name + "','" + tmp_user + "', '" + tmp_password + "'" + ");";
+			Statement st = conn.createStatement();
+			st.execute(newuser_query);
+			
+			nameTextField.setText("");
+			createuserTextField.setText("");
+			newPassword.setText("");
+			
+			PostLogin lg = new PostLogin();
+			lg.setVisible(true);
+			dispose();
+			
+		}catch (SQLException err) {
+			System.out.println(err.getMessage());
+		}
 	}
 }
-
-
-
-
-
-
-
